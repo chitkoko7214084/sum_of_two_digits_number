26092f7d954abd1806bfa6a5d454b9c09bebae77
chit ko <80560570+chitkoko7214084@users.noreply.github.com>
2023-06-29 16:46:09 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+#This program will generate the sum of two digits number
+#Author : Chit Ko Ko
+
+two_digit_number = input("Type a two digit number: ")
+
+
+x = int (two_digit_number[0])
+y= int (two_digit_number[1])
+
+sum = x+y
+
+print (sum)
